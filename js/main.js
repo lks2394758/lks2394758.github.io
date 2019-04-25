971bdffcc199c1bb1fcf350b222ad30324108c8d
Tony <tony@goopter.com>
2019-04-25 15:57:41 -0700

    update

--- js/main.js
+++ js/main.js
@@ -310,21 +310,29 @@ AOS.init({
         alert('Please enter all infomation to reserve a table');
       }
   });
-})(jQuery);
+  
 
-// var bgImageArray = ['images/headImage.jpeg','images/headImage2.jpeg','images/headImage3.jpeg'];
-// var secs = 4;
-// bgImageArray.forEach(function(img){
-//   new Image().src = img;
-// }); 
-// function backgroundSequence() {
-//   window.clearTimeout();
-//   var k = 0;
-//   for (i = 0; i < bgImageArray.length; i++) {
-//     setTimeout(function(){ 
-//       document.getElementById("cover_bg").style.background = "url(" + bgImageArray[k] + ")";
-//     if ((k + 1) === bgImageArray.length) { setTimeout(function() { backgroundSequence() }, (secs * 1000))} else { k++; }			
-//     }, (secs * 1000) * i)	
-//   }
-// }
-// backgroundSequence();
+  var set_locale_to = function(locale) {
+    if (locale)
+      $.i18n().locale = locale;
+  };
+
+  $.i18n().load({
+    'en':'../languages/en.json',
+    'zh':'../languages/zh.json'
+  }).done(function(){
+    $('#enclick').click(function(e){
+      set_locale_to('en');
+      $('body').i18n();
+      e.preventDefault();
+      $.i18n().locale = $(this).data('locale');
+    });
+
+    $('#zhclick').click(function(e){
+      set_locale_to('zh');
+      $('body').i18n();
+      e.preventDefault();
+      $.i18n().locale = $(this).data('locale');
+    });
+  });
+})(jQuery);
